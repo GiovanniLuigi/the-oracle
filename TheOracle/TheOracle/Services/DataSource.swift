47540daf373f249d873c4ec14d81d2ae9edde8d4
GiovanniLuigi <giovannibruno128@gmail.com>
2021-01-18 10:25:30 -0300

    added project structure

---
+++ TheOracle/TheOracle/Services/DataSource.swift
@@ -0,0 +1,9 @@
+//
+//  DataSource.swift
+//  TheOracle
+//
+//  Created by Giovanni Luidi Bruno on 18/01/21.
+//  Copyright © 2021 Giovanni Luigi Bruno. All rights reserved.
+//
+
+import Foundation
---
+++ TheOracle/TheOracle/Services/Network.swift
@@ -0,0 +1,9 @@
+//
+//  Network.swift
+//  TheOracle
+//
+//  Created by Giovanni Luidi Bruno on 18/01/21.
+//  Copyright © 2021 Giovanni Luigi Bruno. All rights reserved.
+//
+
+import Foundation
