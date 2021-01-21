9e984e417764a10b37f8f2bd563129e69b148f86
GiovanniLuigi <giovannibruno128@gmail.com>
2021-01-21 18:33:59 -0300

    added firebase

---
+++ TheOracle/TheOracle/Model/Oracle.swift
@@ -0,0 +1,16 @@
+//
+//  Oracle.swift
+//  TheOracle
+//
+//  Created by Giovanni Luidi Bruno on 21/01/21.
+//  Copyright © 2021 Giovanni Luigi Bruno. All rights reserved.
+//
+
+import Foundation
+
+
+struct Oracle: Codable {
+    let title: String
+    let imageUrl: String
+    let rules: String
+}
